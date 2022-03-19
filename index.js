75c56622fdca066bb3cc3dfc25741071a64720c5
Pritam <pritam42069@protonmail.com>
2022-03-19 14:46:21 +0530

    fixed paths

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var app = express()
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public/'))
 
 var server = app.listen(8081, function() {
     var port = server.address().port;
