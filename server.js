ca5f4d9d3d8a1336c05c24c3fe6152ed91f275b1
Pritam <pritam42069@protonmail.com>
2022-03-19 14:19:05 +0530

    fuck

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require("express");
+
 var app = express()
+
 app.use(express.static('public'))
+
 var server = app.listen(8081, function() {
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
+});
+
+app.get('/test', (req, res) => {
+    res.sendFile(__dirname + '/pages/general/blogs.html');
 });
