b6c53e57670af81b1cd760f5d5504d8d05c69966
Pritam <pritam42069@protonmail.com>
2022-03-19 14:41:55 +0530

    added vercel stuff

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ var server = app.listen(8081, function() {
     console.log("Server started at http://localhost:%s", port);
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 app.get('/test', (req, res) => {
     res.sendFile(__dirname + '/public/pages/general/blogs.html');
 });
