bc2fbfc3972bd0322ec284c1be52f0a38abd4b0e
Pritam <pritam42069@protonmail.com>
2022-03-20 11:35:24 +0530

    added some stuff to track views

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-var express = require("express");
-var fs = require('fs')
+const express = require("express");
+const { MongoClient } = require('mongodb');
+const databases = require("./public/js/databases.js")
 
-var app = express()
+var app = express();
 
-app.use(express.static(__dirname + '/public/'))
+app.use(express.static(__dirname + '/public/'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/pages/general/index.html');
+    databases.addViews().catch(console.error);
+});
+
+app.get('/blogs', (req, res) => {
+    console.log(req.socket.remoteAddress);
+    res.sendFile(__dirname + '/public/pages/general/blogs.html');
+    databases.addViews();
+});
 
 var server = app.listen(8081, function() {
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-app.get('/blogs', (req, res) => {
-    res.sendFile(__dirname + '/public/pages/general/blogs.html');
 });
---
+++ public/js/databases.js
@@ -0,0 +1,19 @@
+const {MongoClient} = require('mongodb')
+
+const url = "mongodb+srv://pritam:" + encodeURIComponent(`${process.env.MONGO_PASS}`) + "@cluster0.w2y5k.mongodb.net/main?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+
+async function addViews() {
+    try {
+        await client.connect();
+        const db = client.db("main");
+        const collection = db.collection("views");
+        await collection.updateOne({_id:"views"}, {$inc:{total:1}});
+    } catch (e) {
+        console.log(e);
+    } finally {
+        client.close();
+    }
+}
+
+module.exports = {addViews};
