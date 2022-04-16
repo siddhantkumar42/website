5ac5cabdeca51fba384fbb730ebbe95bb2ca4b37
Pritam <pritam42069@protonmail.com>
2022-04-16 13:58:25 +0530

    added form

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { Webhook, MessageBuilder } = require('discord-webhook-node');
 
 var app = express();
 
+console.log(process.env.WEBHOOK_URL)
+
 app.use(express.static(__dirname + '/public/'));
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/pages/general/index.html');
@@ -12,6 +17,25 @@ app.get('/blogs', (req, res) => {
     console.log(req.socket.remoteAddress);
     res.sendFile(__dirname + '/public/pages/general/blogs.html');
 });
+
+app.get('/mod', (req, res) => {
+    console.log(req.socket.remoteAddress);
+    res.sendFile(__dirname + '/public/pages/temp/mod_app.html');
+});
+
+app.get('/mod_submit', (req, res) => {
+    const webhook_url = process.env.WEBHOOK_URL;
+    const hook = new Webhook(webhook_url);
+    hook.setUsername("Mod Application");
+    const embed = new MessageBuilder()
+        .setTitle(req.query.username)
+        .addField('Country', req.query.country, true)
+        .addField('GitHub', req.query.github, true)
+        .setColor('#00b0f4')
+        .setTimestamp();
+    hook.send(embed)
+    return res.sendFile(__dirname + '/public/pages/general/index.html');
+})
 
 app.get("*", (req, res) => {
     res.status(404).sendFile(__dirname + "/public/pages/general/404_not_found.html");
