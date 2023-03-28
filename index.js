4b27920173b6c3a57831e3dd1055860de70538c6
pritam <pritam42069@protonmail.com>
2023-03-28 21:38:12 +0530

    edited code

--- index.js
+++ index.js
@@ -4,48 +4,42 @@ const bodyParser = require("body-parser");
 const { Webhook, MessageBuilder } = require('discord-webhook-node');
 
 var app = express();
+var views = 0;
 
-app.use(
-    session ({
-        secret: "ye kya hota hai?",
-        resave: true,
-        saveUninitialized: false
-    })
-)
+function addView(num = 1) {
+    views += num;
+}
+
 app.use(express.static(__dirname + '/public/'));
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     console.log(req.socket.remoteAddress);
     res.sendFile(__dirname + '/public/pages/general/index.html');
-    req.session.views++;
+    addView();
 });
 
 app.get('/blogs', (req, res) => {
     console.log(req.socket.remoteAddress);
     res.sendFile(__dirname + '/public/pages/general/blogs.html');
-    req.session.views++;
+    addView();
 });
 
 app.get('/resources', (req, res) => {
     console.log(req.socket.remoteAddress);
     res.sendFile(__dirname + '/public/pages/blogs/resources.html');
-    req.session.views++;
+    addView();
 });
 
 app.get("/views", function(req, res, next) {
-    if (req.session.views) {
-        req.session.views++;
-        res.write(`${req.session.views}`);
-        res.end();
-    } else {
-        req.session.views = 1;
-        res.end('Initialised.');
-    }
+    res.write(`${views}`);
+    res.end();
+    addView();
 })
 
 app.get("*", (req, res) => {
     res.status(404).sendFile(__dirname + "/public/pages/general/404_not_found.html");
+    addView();
 })
 
 var server = app.listen(8081, function() {
