2b155a868c0a3442dc829161f4cc31d73276435f
pritam <pritam42069@protonmail.com>
2023-03-29 21:54:13 +0530

    edited code

--- index.js
+++ index.js
@@ -2,47 +2,49 @@ const express = require("express");
 const session = require('express-session')
 const bodyParser = require("body-parser");
 const { Webhook, MessageBuilder } = require('discord-webhook-node');
+require('dotenv').config();
+
+const hook = new Webhook(process.env.webhook)
 
 var app = express();
 var views = 0;
 
-function addView(num = 1) {
-    views += num;
+function addView(ip) {
+    hook.send(ip)
+    views += 1;
 }
 
 app.use(express.static(__dirname + '/public/'));
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
+    addView(`${req.socket.remoteAddress}`);
     console.log(req.socket.remoteAddress);
     res.sendFile(__dirname + '/public/pages/general/index.html');
-    addView();
 });
 
 app.get('/blogs', (req, res) => {
-    console.log(req.socket.remoteAddress);
+    addView(req.socket.remoteAddress);
     res.sendFile(__dirname + '/public/pages/general/blogs.html');
-    addView();
 });
 
 app.get('/resources', (req, res) => {
-    console.log(req.socket.remoteAddress);
+    addView(req.socket.remoteAddress);
     res.sendFile(__dirname + '/public/pages/blogs/resources.html');
-    addView();
 });
 
-app.get("/views", function(req, res, next) {
+app.get("/views", function (req, res, next) {
+    addView(req.socket.remoteAddress);
     res.write(`${views}`);
     res.end();
-    addView();
 })
 
 app.get("*", (req, res) => {
+    addView(req.socket.remoteAddress);
     res.status(404).sendFile(__dirname + "/public/pages/general/404_not_found.html");
-    addView();
 })
 
-var server = app.listen(8081, function() {
+var server = app.listen(8081, function () {
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
 });
