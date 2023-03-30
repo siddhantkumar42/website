9f953a2b369c37618fc9f0937965c9c08e15294a
pritam <pritam42069@protonmail.com>
2023-03-30 22:45:46 +0530

    testtign

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { Webhook, MessageBuilder } = require('discord-webhook-node');
+const geoip = require('geoip-country');
 require('dotenv').config();
 
 const hook = new Webhook(process.env.webhook)
@@ -9,7 +10,11 @@ var app = express();
 var views = 0;
 
 function addView(ip) {
-    hook.send(ip)
+    // I am not ip logging, just curious about what country the viewers might be from
+    var geo = geoip.lookup(ip);
+    if (geo) {
+        hook.send(geo);
+    }
     views += 1;
 }
 
@@ -19,34 +24,34 @@ app.set('trust proxy', true)
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/pages/general/index.html');
-    addView(`req.ip: ${req.ip}`);
+    addView(`${req.ip}`);
 });
 
 app.get('/blogs', (req, res) => {
     res.sendFile(__dirname + '/public/pages/general/blogs.html');
-    addView(`req.ip: ${req.ip}`);
+    addView(`${req.ip}`);
 });
 
 app.get('/resources', (req, res) => {
     res.sendFile(__dirname + '/public/pages/blogs/resources.html');
-    addView(`req.ip: ${req.ip}`);
+    addView(`${req.ip}`);
 });
 
 app.get("/views", function (req, res, next) {
     res.write(`${views}`);
     res.end();
-    addView(`req.ip: ${req.ip}`);
+    addView(`${req.ip}`);
 })
 
 app.get("/test", function (req, res, next) {
     console.log(req.header("x-forwarded-for"))
     res.sendFile(__dirname + '/public/pages/general/index.html');
-    addView(`req.ip: ${req.ip}`);
+    addView(`${req.ip}`);
 })
 
 app.get("*", (req, res) => {
     res.status(404).sendFile(__dirname + "/public/pages/general/404_not_found.html");
-    addView(`req.ip: ${req.ip}`);
+    addView(`${req.ip}`);
 })
 
 var server = app.listen(8081, function () {
